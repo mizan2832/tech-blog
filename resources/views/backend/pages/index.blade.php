8724da8d1bbcab7b0b7243252cd7c6c0f1e7e5fa
Mizanur Rahman <mrahman5037@gmail.com>
2023-07-10 16:53:01 +0600

    sec

---
+++ resources/views/backend/pages/index.blade.php
@@ -0,0 +1,5 @@
+@extends('backend.master')
+@section('title','ADMIN TEMPLATE')
+@section('content')
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('front.pages.index');
+    return view('backend.pages.index');
 });
